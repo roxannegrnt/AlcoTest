d8d1a4aa073548953e6c22c295dfb302429928b6
roxannegrnt <roxanne.grnt@eduge.ch>
2017-02-07 08:45:03 +0100

    Création du Modèle + fonction boire

--- AlcoTest/AlcoTest/ModeleAlcoTest.cs
+++ AlcoTest/AlcoTest/ModeleAlcoTest.cs
@@ -8,5 +8,80 @@ namespace AlcoTest
 {
     class ModeleAlcoTest
     {
+        private int _masse;
+        private int _sexe;
+        private int _clitre;
+        private List<string> _alcFav;
+        private List<string> _toutAlc;
+        private double _TauxAlc;
+        private int _timer;
+
+        public int Masse
+        {
+            get { return _masse; }
+            set { _masse = value; }
+        }
+
+        public int Sexe
+        {
+            get { return _sexe; }
+            set { _sexe = value; }
+        }
+        public int Litre
+        {
+            get { return _clitre; }
+            set { _clitre = value; }
+        }
+        public List<string> AlcFav
+        {
+            get { return _alcFav; }
+            set { _alcFav = value; }
+        }
+
+        public List<string> ToutAlc
+        {
+            get { return _toutAlc; }
+            set { _toutAlc = value; }
+        }
+
+        public double TauxAlc
+        {
+            get { return _TauxAlc; }
+            set { _TauxAlc = value; }
+        }
+
+        public int Timer
+        {
+            get { return _timer; }
+            set { _timer = value; }
+        }
+        public ModeleAlcoTest()
+        {
+            this.TauxAlc = 0;
+        }
+        public double Boire()
+        {
+            //TauxGenre takes 0.6 if gender is Female and 0.7 if gender is Male.
+            double Tauxgenre= ((this.Sexe==0) ? 0.6 : 0.7);
+            double gramme= this.Litre;
+           this.TauxAlc += gramme / (this.Masse * Tauxgenre);
+           return this.TauxAlc;
+        }
+        public void Rafraichir()
+        {
+
+        }
+        public void DessineGraphique()
+        {
+
+        }
+        public void SauverData()
+        {
+
+        }
+        public void AfficherAlcool()
+        {
+
+        }
     }
 }
